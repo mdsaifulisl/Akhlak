9340be781480baa7ee80da1532dc9d06a30c305c
Md Saiful Islam <156702295+mdsaifulisl@users.noreply.github.com>
2025-10-28 15:36:36 +0600

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,24 +3,19 @@ const mySQL = require("mysql2");
 
 
 
-const db = mySQL.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "akhlak",
-});
-
 // const db = mySQL.createConnection({
 //     host: "localhost",
-//     user: "reliable_akhlak",
-//     password: "{jamif,,f&q08v]!",
-//     database: "reliable_akhlak",
+//     user: "root",
+//     password: "root",
+//     database: "akhlak",
 // });
 
-
-
-
-
+const db = mySQL.createConnection({
+    host: "localhost",
+    user: "reliable_akhlak",
+    password: "{jamif,,f&q08v]!",
+    database: "reliable_akhlak",
+});
 
 
 db.connect((err) => {
@@ -32,3 +27,4 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
